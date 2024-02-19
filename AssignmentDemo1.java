aeb15188de8583176bbde5a9bd98c31a0fa30eb9
vinishakonda <154752645+vinishakonda@users.noreply.github.com>
2024-02-19 18:31:06 +0530

    Add files via upload

---
+++ AssignmentDemo1.java
@@ -0,0 +1,45 @@
+package courseEndProject;
+
+import java.io.File;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class AssignmentDemo2 {
+
+	Logger logger = LogManager.getLogger(AssignmentDemo2.class);
+
+	@Test(priority = '1')
+	public void assignment002Post() {
+		logger.info("Course End project - Assignment002 - POST request");
+		File file = new File(
+				"C:\\Users\\farha\\eclipse-workspace\\Rest-API\\Phase-3-rest-API\\scr\\main\\resource\\data.json");
+		int id = RestAssured.given().baseUri("https://petstore.swagger.io/v2/pet").contentType(ContentType.JSON)
+				.body(file).when().post().then().statusCode(200).log().all().body("name", Matchers.equalTo("Doggie"))
+				.extract().path("id");
+		logger.trace("The status code is checked");
+
+		System.out.println(id);
+
+		logger.trace("Id has been captured and validated");
+
+	}
+
+	@Test(priority = '2', dependsOnMethods = "assignment002Post")
+	public void assignment002Put() {
+		File file = new File(
+				"C:\\Users\\farha\\eclipse-workspace\\Rest-API\\Phase-3-rest-API\\scr\\main\\resource\\dataput.json");
+		int id = RestAssured.given().baseUri("https://petstore.swagger.io/v2/pet/").contentType(ContentType.JSON)
+				.body(file).when().put().then().statusCode(200).log().all()
+				.body("status", Matchers.equalTo("available_QA")).extract().path("id");
+
+		System.out.println(id);
+
+	}
+
+}
---
+++ AssignmentDemoCombo.java
@@ -0,0 +1,90 @@
+package courseEndProject;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class AssignmentDemoCombo {
+	
+	@Test(priority='1')
+	public void assignment003User()
+	{
+		RestAssured.given()
+		.baseUri("https://petstore.swagger.io/v2/user/Uname001")
+		.when()
+		.get()
+		.then()
+		.statusCode(200)
+		.log().all()
+		.body("username", Matchers.equalTo("Uname001"))
+		.body("email", Matchers.equalTo("Positive@Attitude.com"))
+		.body("userStatus", Matchers.equalTo(1))
+		;
+	
+		
+		
+	}
+	
+	
+	@Test(priority='2')
+	public void assignment004login()
+	{
+		RestAssured.given()
+		.baseUri("https://petstore.swagger.io/v2/user/login")
+		.auth().preemptive().basic("Uname001", "@tt!tude")
+		.when()
+		.get()
+		.then()
+		.statusCode(200)
+		.log().all()
+		.body("message", Matchers.anything());
+	}
+		
+		
+		@Test(priority='3')
+		public void assignment005FindByStatus()
+		{
+			RestAssured.given()
+			.baseUri("https://petstore.swagger.io/v2/pet/findByStatus")
+			//.queryParam("status", "available")
+			//.queryParam("status", "pending")
+			.queryParam("status", "sold")
+			.when()
+			.get()
+			.then()
+			.statusCode(200)
+			.log().all();
+	
+		
+			
+			
+		}
+		
+		
+
+		@Test(priority='4')
+		public void assignment006Logout()
+		{
+			RestAssured.given()
+			.baseUri("https://petstore.swagger.io/v2/user/logout")
+			.when()
+			.get()
+			.then()
+			.statusCode(200)
+			.log().all()
+			.body("code", Matchers.equalTo(200))
+			.body("type", Matchers.equalTo("unknown"))
+			.body("message", Matchers.equalTo("ok"))
+			;
+		
+			
+			
+		}
+		
+
+	
+		
+		
+	}
+
